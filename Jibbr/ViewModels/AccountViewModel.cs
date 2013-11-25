7610d2d6fd44d46f8c31ebca81ed919bacf4008c
Nimgoble <nimgoble@gmail.com>
2013-11-25 16:07:11 -0600

    Added Account ViewModel

---
+++ Jibbr/ViewModels/AccountViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Caliburn.Micro;
+namespace Jibbr.ViewModels
+{
+    class AccountViewModel : Screen
+    {
+        public AccountViewModel()
+        {
+        }
+
+        #region Properties
+
+        private String username;
+        public String UserName
+        {
+            get { return username; }
+            set
+            {
+                if (value == username)
+                    return;
+
+                username = value;
+                NotifyOfPropertyChange(() => UserName);
+            }
+        }
+
+        private String password;
+        public String Password
+        {
+            get { return password; }
+            set
+            {
+                if (value == password)
+                    return;
+
+                password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
+        public ICommand SignIn { get; protected set; }
+
+        #endregion
+    }
+}
--- Jibbr/ViewModels/MainViewModel.cs
+++ Jibbr/ViewModels/MainViewModel.cs
@@ -1,54 +1,27 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Caliburn.Micro;
+using ReactiveUI;
+
 namespace Jibbr.ViewModels
 {
     public class MainViewModel : Screen
     {
+        //private ObservableCollection
         public MainViewModel()
         {
             DisplayName = "Jibbr";
-
-            //SignIn = new 
+            //AddAccount = 
         }
 
-        #region Properties
-        private String username;
-        public String UserName
+        //public ICommand AddAccount { get; protected set; }
+        public void AddAccount()
         {
-            get { return username; }
-            set
-            {
-                if (value == username)
-                    return;
-
-                username = value;
-                NotifyOfPropertyChange(() => UserName);
-            }
         }
-
-        private String password;
-        public String Password
-        {
-            get { return password; }
-            set
-            {
-                if (value == password)
-                    return;
-
-                password = value;
-                NotifyOfPropertyChange(() => Password);
-            }
-        }
-
-        public ICommand SignIn { get; protected set; }
-
-        #endregion
     }
-
-    
 }
